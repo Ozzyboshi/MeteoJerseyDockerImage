562e28edb0a11c5d2cf7d7da7e76b9e916c0eaeb
Alessio Garzi <gun101@email.it>
2016-05-20 13:15:56 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-sed -e "s;%TOMCAT_USERNAME%;$TOMCAT_USERNAME;g" /tomcat-users.xml > /usr/local/tomcat/conf/tomcat-users.xml
-sed -e "s;%TOMCAT_PASSWORD%;$TOMCAT_PASSWORD;g" /tomcat-users.xml > /usr/local/tomcat/conf/tomcat-users.xml
+sed  -e "s;%TOMCAT_PASSWORD%;$TOMCAT_PASSWORD;g" -e "s;%TOMCAT_USERNAME%;$TOMCAT_USERNAME;g" /tomcat-users.xml > /usr/local/tomcat/conf/tomcat-users.xml
 catalina.sh run
